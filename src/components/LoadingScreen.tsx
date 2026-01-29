f46ee93f2c5ee1cb030a6deffaf0d59eba3fb0ed
Napolesllll <canojhon148@gmail.com>
2026-01-28 20:27:53 -0500

    Aumento de tiempo de loading de bienvenida

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -11,7 +11,7 @@ const LoadingScreen = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(false);
-    }, 3500);
+    }, 6500);
 
     const handleLoad = () => setIsVisible(false);
     window.addEventListener("load", handleLoad);
